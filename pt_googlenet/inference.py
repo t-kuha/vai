a40cc6afe553cf3e34d5a2f1e0b41678f7cd8344
t-kuha <imagingtechnerd@gmail.com>
2024-11-10 07:16:52 +0000

    Add inference code

---
+++ pt_googlenet/inference.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+import os
+
+import numpy as np
+import torch
+import torchvision
+import tqdm
+import vart
+import xir
+
+
+def get_child_subgraph_dpu(xmodel_path: str) -> list[xir.Subgraph]:
+    """obtain dpu subgrah.
+    """
+    assert os.path.exists(xmodel_path)
+
+    graph = xir.Graph.deserialize(xmodel_path)
+    assert graph, '"graph" should not be None.'
+
+    root_subgraph = graph.get_root_subgraph()
+    assert root_subgraph, 'Failed to get root subgraph of input Graph object.'
+    if root_subgraph.is_leaf:
+        return []
+
+    child_subgraphs = root_subgraph.toposort_child_subgraph()
+    assert child_subgraphs
+    return [
+        cs for cs in child_subgraphs
+        if cs.has_attr('device') and cs.get_attr('device').upper() == 'DPU'
+    ]
+
+
+XMODEL_PATH = 'googlenet.xmodel'
+
+# dataloader & transoform info
+transform_test = torchvision.models.GoogLeNet_Weights.IMAGENET1K_V1.transforms()
+testset = torchvision.datasets.ImageNet(
+    root='.', split='val', transform=transform_test
+)
+testloader = torch.utils.data.DataLoader(testset, batch_size=1, shuffle=False)
+
+# load .xmodel
+subgraphs = get_child_subgraph_dpu(XMODEL_PATH)
+assert len(subgraphs) == 1  # only one DPU kernel
+
+dpu_runner = vart.Runner.create_runner(subgraphs[0], 'run')
+input_fixpos = dpu_runner.get_input_tensors()[0].get_attr('fix_point')
+input_scale = 2**input_fixpos
+
+inputTensors = dpu_runner.get_input_tensors()
+outputTensors = dpu_runner.get_output_tensors()
+input_ndim = tuple(inputTensors[0].dims)
+pre_output_size = int(outputTensors[0].get_data_size() / input_ndim[0])
+
+output_ndim = tuple(outputTensors[0].dims)
+output_fixpos = outputTensors[0].get_attr('fix_point')
+output_scale = 1 / (2**output_fixpos)
+
+# prepare batch input/output
+inputData = [np.empty(input_ndim, dtype=np.int8, order='C')]
+outputData = [np.empty(output_ndim, dtype=np.int8, order='C')]
+
+num_correct = 0
+for inputs, targets in tqdm.tqdm(testloader):
+    inputData[0] = (inputs * input_scale).to(torch.int8, memory_format=torch.channels_last).numpy()
+
+    # run
+    job_id = dpu_runner.execute_async(inputData, outputData)
+    dpu_runner.wait(job_id)
+    num_correct += (np.argmax(outputData[0]) == targets.item())
+
+print(f'accuracy: {num_correct * 100 / (len(testset))} %')
